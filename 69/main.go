1be1642e1bdb312116359a8c1a9a27cacbf29851
sytgj7896321 <sytgj7896321@gmail.com>
2022-02-08 17:09:03 +0800

    69 done

---
+++ 69/main.go
@@ -0,0 +1,23 @@
+package main
+
+import "fmt"
+
+func main() {
+	x := 110923809127
+	fmt.Println(mySqrt(x))
+}
+
+func mySqrt(x int) int {
+	l, r := 0, x
+	ans := -1
+	for l <= r {
+		mid := l + (r-l)/2
+		if mid*mid <= x {
+			ans = mid
+			l = mid + 1
+		} else {
+			r = mid - 1
+		}
+	}
+	return ans
+}
