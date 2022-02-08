db67821285873ebcfc713fcb4aea08fb04c403f6
sytgj7896321 <sytgj7896321@gmail.com>
2022-02-08 14:30:35 +0800

    27 done

---
+++ 27/main.go
@@ -0,0 +1,22 @@
+package main
+
+import "fmt"
+
+func main() {
+	nums := []int{0, 1, 2, 2, 3, 0, 4, 2}
+	val := 2
+	fmt.Println(removeElement(nums, val))
+}
+
+func removeElement(nums []int, val int) int {
+	left, right := 0, len(nums)
+	for left < right {
+		if nums[left] == val {
+			nums[left] = nums[right-1]
+			right--
+		} else {
+			left++
+		}
+	}
+	return left
+}
